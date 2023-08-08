7fbb7cf6d28cc2b91705ecec1cbc0b5f02bd386d
Eduardo A.M <eduardo.maciel@edu.univali.br>
2023-08-08 08:29:26 -0300

    add custom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import { register } from "swiper/element/bundle";
 
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/scrollbar";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import CarouselShoes from "./components/CarouselShoes";
 import ShowcaseTilt from "./components/ShowcaseTilt";
 
 register();
-
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/scrollbar";
 
 export default function App() {
    return (
