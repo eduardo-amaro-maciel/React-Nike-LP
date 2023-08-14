3be592c8a0cc2e86390184ed0af47fcada1c8843
Eduardo Amaro Maciel <eduardo.maciel@edu.univali.br>
2023-08-14 12:42:04 -0300

    add custom cursor

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import ShowcaseGrid from "./components/ShowcaseGrid";
 import ShowcaseGif from "./components/ShowcaseGif";
 import Faq from "./components/Faq";
 import Footer from "./components/Footer";
+import AnimatedCursor from "react-animated-cursor";
 
 register();
 
@@ -23,6 +24,15 @@ export default function App() {
          <ShowcaseGif />
          <Faq />
          <Footer />
+         <AnimatedCursor
+            innerSize={8}
+            outerSize={32}
+            color="210,242,84"
+            outerAlpha={0.5}
+            innerScale={0.9}
+            outerScale={2}
+            trailingSpeed={5}
+         />
       </>
    );
 }
